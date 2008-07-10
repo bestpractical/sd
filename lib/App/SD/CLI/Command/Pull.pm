fcb94108203f05a4700dbafb65a56abeb4d04ab9
Shawn Moore <sartak@bestpractical.com>
2008-07-10 19:52:37 +0000

    r64070@onn:  sartak | 2008-07-10 15:52:26 -0400  Don't rebless into our parent class, just override/super "run"
    git-svn-id: svn://s.ly/sd/trunk@13960 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command/Pull.pm
+++ lib/App/SD/CLI/Command/Pull.pm
@@ -2,17 +2,17 @@ package App::SD::CLI::Command::Pull;
 use Moose;
 extends qw/App::SD::CLI::Command::Merge/;
 
-sub run {
+override run => sub {
     my $self = shift;
 
     die "Please specify a --from.\n" if !defined($self->args->{'from'});
 
     local $ENV{PROPHET_RESOLVER} = 'Prompt';
-    bless $self, 'App::SD::CLI::Command::Merge';
-    $self->args({  from => $self->args->{'from'},
-                   to => $self->cli->app_handle->default_replica_type.":file://".$self->cli->app_handle->handle->fs_root });
-    $self->run;
-}
+
+    $self->set_arg(to => $self->cli->app_handle->default_replica_type.":file://".$self->cli->app_handle->handle->fs_root);
+
+    super();
+};
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
