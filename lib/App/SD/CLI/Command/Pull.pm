1827cd8c12ad3d1b68375c5028fed5d8cc801ec3
Simon Wistow <simon@thegestalt.org>
2008-07-27 21:26:55 +0000

    Remove unnecessary code (also allows 'pull --all' to work)
    git-svn-id: svn://s.ly/sd/trunk@14550 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command/Pull.pm
+++ lib/App/SD/CLI/Command/Pull.pm
@@ -1,19 +1,6 @@
 package App::SD::CLI::Command::Pull;
 use Moose;
-extends qw/App::SD::CLI::Command::Merge/;
-
-override run => sub {
-    my $self = shift;
-
-    die "Please specify a --from.\n" if !$self->has_arg('from');
-
-    local $ENV{PROPHET_RESOLVER} = 'Prompt';
-
-    $self->set_arg(to => $self->cli->app_handle->default_replica_type.":file://".$self->cli->app_handle->handle->fs_root);
-    $self->set_arg(db_uuid => $self->app_handle->handle->db_uuid);
-
-    super();
-};
+extends qw/Prophet::CLI::Command::Pull/;
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
