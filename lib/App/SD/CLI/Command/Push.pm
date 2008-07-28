5c075fe7f4f83261f5901bc87d2ea1bad179d821
Shawn Moore <sartak@bestpractical.com>
2008-07-28 16:55:03 +0000

    r65317@onn:  sartak | 2008-07-28 12:54:12 -0400  Have push just subclass Prophet's push command
    git-svn-id: svn://s.ly/sd/trunk@14577 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command/Push.pm
+++ lib/App/SD/CLI/Command/Push.pm
@@ -1,19 +1,6 @@
 package App::SD::CLI::Command::Push;
 use Moose;
-extends qw/App::SD::CLI::Command::Merge/;
-
-override run => sub {
-    my $self = shift;
-
-    die "Please specify a --to.\n" if !$self->has_arg('to');
-
-    local $ENV{PROPHET_RESOLVER} = 'Prompt';
-
-    $self->set_arg(from => $self->app_handle->default_replica_type.":file://".$self->app_handle->handle->fs_root);
-    $self->set_arg(db_uuid => $self->app_handle->handle->db_uuid);
-
-    super();
-};
+extends qw/Prophet::CLI::Command::Push/;
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
