22c6ca90edc1c5b4e5af4f8e5a5b4787205666b9
Christine Spang <spang@bestpractical.com>
2008-07-31 11:48:21 +0000

    r47464@loki:  spang | 2008-07-31 12:42:53 +0100  make get_content more flexible
    git-svn-id: svn://s.ly/sd/trunk@14689 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command.pm
+++ lib/App/SD/CLI/Command.pm
@@ -5,10 +5,22 @@ use Path::Class;
 =head2 get_content %args
 
 This is a helper routine for use in SD commands to enable getting records
-in different ways, such as from a file, on the commandline, or from an
+in different ways such as from a file, on the commandline, or from an
 editor. Returns the record content.
 
-Valid keys in %args are type and default_edit => bool.
+Valid keys in %args are type => str, default_edit => bool, and
+prefill_props => $props_hash_ref, props_order => $order_array_ref,
+footer => str, header => str.
+
+Specifying props with prefill_props allows you to present lists of
+key/value pairs (with possible default values) for a user to fill in.
+If you need a specific ordering of props, specify it with
+C<props_order>. Specifying C<header> and/or C<footer> allows you to
+print some optional text before and/or after the list of props.
+
+Note that you will have to post-process C<$content> from the routine
+calling C<get_content> in order to extract the keys and values from
+the returned text.
 
 =cut
 
@@ -20,10 +32,25 @@ sub get_content {
     if (my $file = file($self->delete_arg('file'))) {
         $content = $file->slurp();
         $self->set_prop(name => $file->basename);
-    } elsif ($content = $self->delete_arg('content')) {
+    } elsif ($content = $self->delete_prop('content')) {
 
     } elsif ($args{default_edit} || $self->has_arg('edit')) {
-        $content = $self->edit_text('');
+        my $text = '';
+        if (my $header = $args{header}) {
+            $text .= $header;
+        }
+        if (my $props = $args{prefill_props}) {
+            my @ordering = (my $props_order = $args{props_order}) ?
+                                @$props_order : keys %$props;
+            $text .= join "\n", map { "$_: $props->{$_}" } @ordering;
+        }
+        if (my $footer = $args{footer}) {
+            $text .= $footer;
+        }
+        $content = $self->edit_text($text);
+        # user aborted their text editor without changing anything; signify
+        # this to the caller by returning nothing
+        $content = '' if $content eq $text;
     } else {
         print "Please type your $args{type} and press ctrl-d.\n";
         $content = do { local $/; <> };
