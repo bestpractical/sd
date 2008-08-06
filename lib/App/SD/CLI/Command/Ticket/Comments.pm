9901825ad4e44d6cd75b3136916b7b5c9e611825
Christine Spang <spang@bestpractical.com>
2008-08-06 00:15:06 +0000

    r47749@loki:  spang | 2008-08-06 00:21:16 +0100  simplify the details command code by using attachment/comment references instead of searching through all the attachment/comment records  r47781@loki:  spang | 2008-08-06 00:49:33 +0100  undo that last commit  r47782@loki:  spang | 2008-08-06 00:51:59 +0100  simplify ticket details command code by using comment / attachment references  r47783@loki:  spang | 2008-08-06 00:58:13 +0100  make commands that require uuids and don't inherit from prophet commands that also do fail more gracefully when uuid is not set  r47784@loki:  spang | 2008-08-06 01:13:02 +0100  fix an omission, yay for tests
    git-svn-id: svn://s.ly/sd/trunk@14834 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command/Ticket/Comments.pm
+++ lib/App/SD/CLI/Command/Ticket/Comments.pm
@@ -7,12 +7,14 @@ with 'App::SD::CLI::Model::Ticket';
 sub run {
     my $self = shift;
     my $record = $self->_get_record_class();
+
+    $self->require_uuid;
     $record->load( uuid => $self->uuid );
     unless (@{$record->comments}) {
         print "No comments found\n";
     }
 
-    for my $entry (sort { $a->prop('date') cmp $b->prop('date') } @{$record->comments}) {
+    for my $entry (sort { $a->prop('created') cmp $b->prop('created') } @{$record->comments}) {
          print "id: ".$entry->luid." (".$entry->uuid.")\n";
         print "created: ".$entry->prop('created')."\n";
         print $entry->prop('content')."\n";
