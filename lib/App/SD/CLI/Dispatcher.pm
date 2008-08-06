5b4cddcb3809e6cc771ce2304b712ab8291c6352
Shawn Moore <sartak@bestpractical.com>
2008-08-06 18:05:48 +0000

    r68891@onn:  sartak | 2008-08-06 14:05:36 -0400  Most of the SD dispatcher
    git-svn-id: svn://s.ly/sd/branches/dispatcher@14863 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/App/SD/CLI/Dispatcher.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+package App::SD::CLI::Dispatcher;
+use strict;
+use warnings;
+use Prophet::CLI::Dispatcher -base;
+
+on qr{^(ticket|comment|attachment) \s+ (.*)}xi => sub {
+    my $cli = shift;
+    $cli->set_arg(type => $1);
+    run($2, $cli, @_);
+};
+
+on qr{.} => sub {
+    my $cli = shift;
+    my %args = @_;
+
+    my @pieces = @{ $args{dispatching_on} };
+
+    my @possible_classes;
+    while (@pieces) {
+        push @possible_classes, "App::SD::CLI::Command::"
+                              . join '::', @pieces;
+        shift @pieces;
+    }
+
+    for my $class (@possible_classes) {
+        if ($cli->_try_to_load_cmd_class($class)) {
+            return $args{got_command}->($class);
+        }
+    }
+
+    next_rule;
+};
+
+1;
+
