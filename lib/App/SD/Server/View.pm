8b4d8f26cc3daeec84f55dcc0502f6fd719127d2
Jesse Vincent <jesse@bestpractical.com>
2008-09-08 02:42:34 +0000

    The very beginning of an actual html ui for the bug trakcing side
    git-svn-id: svn://s.ly/sd/trunk@15790 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/Server/View.pm
+++ lib/App/SD/Server/View.pm
@@ -6,11 +6,63 @@ use Template::Declare::Tags;
 use Prophet::Server::ViewHelpers;
 use base 'Prophet::Server::View';
 
+    use App::SD::Model::Ticket;
+    use App::SD::Collection::Ticket;
+
+
 template '/' => 
     page    { 'SD' }
     content {
          p { 'sd is a P2P bug tracking system.' };
 
+    show('/bugs/open');
+
 };
 
+
+template '/bugs/open' => sub {
+    my $self = shift;
+    my $bugs = App::SD::Collection::Ticket->new( app_handle => $self->app_handle, handle     => $self->app_handle->handle);
+    $bugs->matching( sub { my $item = shift; return $item->prop('status') ne 'closed'  ? 1 : 0; } );
+
+
+    h2 { 'Open bugs' };
+
+    for my $bug (@$bugs) {
+    ul {
+    
+        li {
+
+
+            bug_link($bug => $bug->luid);
+            span { $bug->prop('status') };
+            span { $bug->prop('summary') };
+            span { $bug->prop('created') };
+
+        }
+
+    }
+
+        }
+
+};
+
+
+template '/show_bug' => page {
+    
+
+} content {
+
+
+};
+
+
+sub bug_link {
+        my $bug = shift;
+        my $label = shift;
+        a {{ class is 'bug';
+            href is '/bug/'.$bug->uuid; };
+            $label;
+        };
+    } 
 1;
