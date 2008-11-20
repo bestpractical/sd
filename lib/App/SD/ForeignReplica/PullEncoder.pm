84ccc1092def598da01d592885c2a7aadc4064e1
Ruslan Zakirov <ruz@bestpractical.com>
2008-11-20 17:08:06 +0000

    * fix uninit warnings
    git-svn-id: svn://s.ly/sd/trunk@16955 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/ForeignReplica/PullEncoder.pm
+++ lib/App/SD/ForeignReplica/PullEncoder.pm
@@ -2,12 +2,13 @@ package App::SD::ForeignReplica::PullEncoder;
 use Moose;
 
 sub warp_list_to_old_value {
-    my $self         = shift;
-    my $current_value = shift ||'';
-    my $add          = shift;
-    my $del          = shift;
+    my $self    = shift;
+    my $current = shift;
+    my $add     = shift;
+    my $del     = shift;
+    $_ = '' foreach grep !defined, $current, $add, $del;
 
-    my @new = grep defined && length, split /\s*,\s*/, $current_value;
+    my @new = grep defined && length, split /\s*,\s*/, $current;
     my @old = grep defined && length && $_ ne $add, (@new, $del);
     return join( ", ", @old );
 }
