1bd8e460fd9b1b7c38ecace2cabb7e696889e06c
Jesse Vincent <jesse@bestpractical.com>
2008-12-23 00:28:58 +0000

    * Added a 'project name' property
    git-svn-id: svn://s.ly/sd/trunk@17305 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD.pm
+++ lib/App/SD.pm
@@ -20,8 +20,10 @@ sub database_settings {
         components          => ['6CBD84A1-4568-48E7-B90C-F1A5B7BD8ECD' => qw/core ui docs tests/],
         default_component   => ['0AEC922F-57B1-44BE-9588-816E5841BB18' => 'core'],
         milestones          => ['1AF5CF74-A6D4-417E-A738-CCE64A0A7F71' => qw/alpha beta 1.0/],
-        default_milestone   => ['BAB613BD-9E25-4612-8DE3-21E4572859EA' => 'alpha']
-};
+        default_milestone   => ['BAB613BD-9E25-4612-8DE3-21E4572859EA' => 'alpha'],
+
+        project_name        => ['3B4B297C-906F-4018-9829-F7CC672274C9' => 'Your SD Project']
+    };
 
 }
 
