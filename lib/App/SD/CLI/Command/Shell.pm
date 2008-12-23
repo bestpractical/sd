46de4d3eac77ca82b5977cbe6a21c28715cafbfc
Jesse Vincent <jesse@bestpractical.com>
2008-12-23 00:29:23 +0000

    * use the new project name property
    git-svn-id: svn://s.ly/sd/trunk@17306 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/App/SD/CLI/Command/Shell.pm
+++ lib/App/SD/CLI/Command/Shell.pm
@@ -3,8 +3,9 @@ use Moose;
 extends 'Prophet::CLI::Command::Shell';
 
 sub preamble {
+    my $self = shift;
     return join "\n",
-        "SD ($App::SD::VERSION; Prophet $Prophet::VERSION)",
+        "SD for ".$self->app_handle->setting( label => 'project_name' )->get()->[0]." ($App::SD::VERSION; Prophet $Prophet::VERSION)",
         'Type "help", "about", or "copying" for more information.',
 }
 
