76e9fb05f66d247564ffff32832c485bd0b49a72
Christine Spang <spang@mit.edu>
2009-01-15 00:03:08 +0200

    test script regexes can be cooler with lookbehinds

--- t/scripts/ticket-create-editor.pl
+++ t/scripts/ticket-create-editor.pl
@@ -3,8 +3,7 @@
 # perl script to trick Proc::InvokeEditor with for the ticket create command
 
 while (<>) {
-     s/^summary:.*$/summary: creating tickets with an editor is totally awesome/;
+     s/(?<=^summary: ).*$/creating tickets with an editor is totally awesome/;
      print;
      print "We can create a comment at the same time.\n" if /^===/;
 }
-
