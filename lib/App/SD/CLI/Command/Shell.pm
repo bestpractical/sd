7b169abe5811031f4ec6fe919abc31204e9e9d87
Jesse Vincent <jesse@bestpractical.com>
2009-01-25 17:40:08 -0500

    Get the project name into the shell

--- lib/App/SD/CLI/Command/Shell.pm
+++ lib/App/SD/CLI/Command/Shell.pm
@@ -2,12 +2,25 @@ package App::SD::CLI::Command::Shell;
 use Moose;
 extends 'Prophet::CLI::Command::Shell';
 
+has project_name => (
+    isa => 'Str',
+    is => 'rw',
+    default => sub { shift->app_handle->setting( label => 'project_name' )->get()->[0]; }
+    );
+
 sub preamble {
     my $self = shift;
     return join "\n",
-        "SD for ".$self->app_handle->setting( label => 'project_name' )->get()->[0]." ($App::SD::VERSION; Prophet $Prophet::VERSION)",
+        "SD for ".$self->project_name." ($App::SD::VERSION; Prophet $Prophet::VERSION)",
         'Type "help", "about", or "copying" for more information.',
 }
+
+sub prompt {
+    my $self = shift;
+
+    return $self->project_name.">";
+}
+
 
 1;
 
