6d1bccdbbf5ba6243e8ab5368f8ddc31b5d2f0dc
Jesse Vincent <jesse@bestpractical.com>
2009-02-09 23:01:17 -0500

    removal of Class::Method::Modifiers changed how lazy we need to be - rafl++

--- lib/App/SD/CLI/Command/Shell.pm
+++ lib/App/SD/CLI/Command/Shell.pm
@@ -3,37 +3,40 @@ use Any::Moose;
 extends 'Prophet::CLI::Command::Shell';
 
 has project_name => (
-    isa => 'Str',
-    is => 'rw',
-    default => sub { my $self = shift;
-                     if ($self->app_handle->handle->replica_exists) {
-                            return $self->app_handle->setting( label => 'project_name' )->get()->[0]; 
-                        } else {
-                            'No database found';
-                        }
-                        }
-    );
+    isa     => 'Str',
+    is      => 'rw',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        if ( $self->app_handle->handle->replica_exists ) {
+            return $self->app_handle->setting( label => 'project_name' )->get()->[0];
+        } else {
+            return 'No database found';
+        }
+    }
+);
 
 sub preamble {
     my $self = shift;
-     my @out = (   "SD for ".$self->project_name." ($App::SD::VERSION; Prophet $Prophet::VERSION)",
-        'Type "help", "about", or "copying" for more information.');
+    my @out  = (
+        "SD for " . $self->project_name . " ($App::SD::VERSION; Prophet $Prophet::VERSION)",
+        'Type "help", "about", or "copying" for more information.'
+    );
 
-    if (!$self->app_handle->handle->replica_exists) {
-        push @out, '', "No SD database was found at ".$self->app_handle->handle->url(),
-        'Type "help init" and "help environment" for tips on how to sort that out.'
+    if ( !$self->app_handle->handle->replica_exists ) {
+        push @out, '', "No SD database was found at " . $self->app_handle->handle->url(),
+            'Type "help init" and "help environment" for tips on how to sort that out.';
     }
 
-    return join("\n",@out);
+    return join( "\n", @out );
 
 }
 
 sub prompt {
     my $self = shift;
 
-    return $self->project_name."> ";
+    return $self->project_name . "> ";
 }
-
 
 1;
 
