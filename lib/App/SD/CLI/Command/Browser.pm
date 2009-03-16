89d7d7c6d563a8d2d2da4e16972d811e1e716594
Shawn M Moore <sartak@gmail.com>
2009-03-16 13:57:23 -0400

    Include /etc/alternatives/www-browser for Debian-based systems

--- lib/App/SD/CLI/Command/Browser.pm
+++ lib/App/SD/CLI/Command/Browser.pm
@@ -27,7 +27,9 @@ sub open_url_cmd {
     } elsif ( $^O eq 'MSWin32' ) {
         return 'start';
     }
-    for my $cmd (qw|www-browser htmlview gnome-open gnome-moz-remote 
+
+    for my $cmd (qw|www-browser htmlview /etc/alternatives/www-browser
+                    gnome-open gnome-moz-remote 
                     firefox iceweasel opera w3m lynx|) {
         my $cmd_path = `which $cmd`;
         chomp($cmd_path);
