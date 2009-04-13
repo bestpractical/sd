40901580c3fed64bb4f5d3177eea389d1943baef
Jesse Vincent <jesse@bestpractical.com>
2009-04-13 10:09:50 -0400

    Reordered sd help environment a bit

--- lib/App/SD/CLI/Command/Help/Environment.pm
+++ lib/App/SD/CLI/Command/Help/Environment.pm
@@ -12,9 +12,8 @@ print <<EOF
 The following environmental variables can be set to affect SD's
 configuration. Example syntax is for bash-like shells.
 
-    export PROPHET_DEVEL=1
-      Turn on various development mode checks, warnings and autoreloading
-      of modules.
+    export SD_REPO=/path/to/sd/replica
+      Specify where the ticket database SD is using should reside.
 
     export PROPHET_USER=name
       Use 'name' as the creator of changesets.
@@ -22,13 +21,6 @@ configuration. Example syntax is for bash-like shells.
     export EMAIL=jesse\@example.com
       Use jesse\@example.com as the default email address for reporting 
       tickets.
-
-    export PROPHET_REPLICA_TYPE=prophet
-      Use the prophet native replica type. Other backend replica
-      types are: sqlite.
-
-    export SD_REPO=/path/to/sd/replica
-      Specify where the ticket database SD is using should reside.
 
     export SD_CONFIG=/path/to/sd/config/file
       Specify where the configuration file SD is using should reside.
@@ -38,6 +30,15 @@ configuration. Example syntax is for bash-like shells.
 
     export PROPHET_HISTLENGTH=100
       Specify how much history the interactive shell should store.
+
+    export PROPHET_DEVEL=1
+      Turn on various development mode checks, warnings and autoreloading
+      of modules.
+
+    export PROPHET_REPLICA_TYPE=prophet
+      Use the prophet native replica type. Other backend replica
+      types are: sqlite.
+
 
 For information on SD database configuration files, see '${cmd}help config'.
 EOF
