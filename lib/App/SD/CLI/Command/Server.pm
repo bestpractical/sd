397572078a284ccbeec030bf6a7744ed9e6d02ea
Jesse Vincent <jesse@bestpractical.com>
2009-04-13 15:44:19 -0400

    Close the pager before starting the SD server

--- lib/App/SD/CLI/Command/Server.pm
+++ lib/App/SD/CLI/Command/Server.pm
@@ -6,6 +6,8 @@ sub run {
     my $self = shift;
     my $server = $self->setup_server();
     $server->read_only(1) unless ($self->has_arg('writable'));
+    Prophet::CLI->end_pager();
+
     $server->run;
 }   
 
