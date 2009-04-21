1e1a24145a29f17e9c237089ad64a5561055f5ca
Jesse Vincent <jesse@bestpractical.com>
2009-04-21 23:57:32 +0900

    Beginning of a sketch of the github replica type From c9s++

---
+++ lib/App/SD/Replica/github.pm
@@ -0,0 +1,63 @@
+package App::SD::Replica::github;
+use Any::Moose;
+extends qw/App::SD::ForeignReplica/;
+
+use Params::Validate qw(:all);
+use Memoize;
+
+use UNIVERSAL::require;
+use URI;
+use Memoize;
+use Net::Github;
+
+use Prophet::ChangeSet;
+
+use constant scheme => 'github';
+use constant pull_encoder => 'App::SD::Replica::github::PullEncoder';
+use constant push_encoder => 'App::SD::Replica::github::PushEncoder';
+
+has github     => ( isa => 'Net::Github', is => 'rw' );
+has remote_url => ( isa => 'Str',         is => 'rw' );
+has owner      => ( isa => 'Str',         is => 'rw' );
+has repo       => ( isa => 'Str',         is => 'rw' );
+
+sub BUILD {
+    my $self = shift;
+
+    my ( $server , $owner , $repo  ) = $self->{url} =~ m/^github:(.+?)\|(\w+)\|(\w+)\|$/
+        or die "Can't parse Github server spec. Expected github:http://user\@github.com|owner|repository|";
+
+
+    my $uri = URI->new($server);
+    my ( $username, $apikey );
+    if ( my $auth = $uri->userinfo ) {
+        ( $username, $apikey ) = split /:/, $auth, 2;
+        $uri->userinfo(undef);
+    }
+
+    ( $username, $apikey ) = $self->prompt_for_login( $uri, $username ) unless $apikey ;
+
+    $self->remote_url("$uri");
+    $self->owner( $owner );
+    $self->repo( $repo );
+
+    $self->github(
+        Net::GitHub->new(
+            login => $username,
+            token => $apikey,
+            repo  => $repo,
+            owner => $owner,
+        ) );
+}
+
+sub record_pushed_transactions {}
+
+sub uuid {
+    my $self = shift;
+    Carp::cluck "- can't make a uuid for this" unless ($self->remote_url && $self->owner && $self->repo );
+    return $self->uuid_for_url( join( '/', $self->remote_url, $self->owner , $self->repo ) );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Any::Moose;
+1;
