73763bcf220d16513fb3999110dec5545b89df6f
Christine Spang <spang@bestpractical.com>
2009-06-25 15:36:47 +0300

    Move SDTestsEditor to Prophet and update tests.
    Settings tests belong in Prophet as well, as SD uses this
    command from Prophet without modification.

--- t/scripts/ticket-update-editor.pl
+++ t/scripts/ticket-update-editor.pl
@@ -1,12 +1,12 @@
 #!perl -i
 use strict;
 use warnings;
-use lib 't/scripts';
-use SDTestsEditor;
+use Prophet::Test::Editor;
 
 # perl script to trick Proc::InvokeEditor with for the ticket update command
 
-SDTestsEditor::edit( tmpl_files => { '--no-args' => 'sd-ticket-update.tmpl',
+Prophet::Test::Editor::edit(
+    tmpl_files => { '--no-args' => 'sd-ticket-update.tmpl',
                    '--all-props' => 'sd-ticket-update-all-props.tmpl',
                    '--verbose' => 'sd-ticket-update-verbose.tmpl',
                    '--verbose-and-all' =>
@@ -38,7 +38,7 @@ SDTestsEditor::edit( tmpl_files => { '--no-args' => 'sd-ticket-update.tmpl',
         if ( /^=== add new ticket comment below ===$/) {
             my $errors = [];
             my $template_ok =
-                SDTestsEditor::check_template_by_line($args{template},
+                Prophet::Test::Editor::check_template_by_line($args{template},
                 $args{valid_template}, $args{replica_uuid},
                 $args{ticket_uuid}, $errors);
             if ($template_ok) {
