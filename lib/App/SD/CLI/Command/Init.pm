21e30bf244bcacdff7cd83aeaa3ab59b2f2c3c78
Christine Spang <spang@bestpractical.com>
2009-08-19 18:33:59 +0100

    Minor fix for init command, I didn't end up using this API

--- lib/App/SD/CLI/Command/Init.pm
+++ lib/App/SD/CLI/Command/Init.pm
@@ -6,11 +6,11 @@ with 'App::SD::CLI::NewReplicaCommand';
 override run => sub {
     my $self = shift;
 
-    my $create_successful = $self->SUPER::run();
+    $self->SUPER::run();
 
     Prophet::CLI->end_pager();
 
-    $self->new_replica_wizard() if $create_successful;
+    $self->new_replica_wizard();
 };
 
 __PACKAGE__->meta->make_immutable;
