f3c6ac313c3f948bfb5043250ed0ea981cbee945
Christine Spang <spang@bestpractical.com>
2009-08-22 14:53:33 +0100

    Update usage messages for init/clone --non-interactive
    Closes: 49bcc92f-8dab-11de-820f-a0a7844892fa

--- lib/App/SD/CLI/Command/Init.pm
+++ lib/App/SD/CLI/Command/Init.pm
@@ -3,6 +3,19 @@ use Any::Moose;
 extends 'Prophet::CLI::Command::Init';
 with 'App::SD::CLI::NewReplicaCommand';
 
+sub usage_msg {
+    my $self = shift;
+    my $cmd = $self->cli->get_script_name;
+
+    return <<"END_USAGE";
+usage: ${cmd}init [--non-interactive]
+
+Options:
+    -n | --non-interactive - Don't prompt to edit settings or specify email
+                             address for new database
+END_USAGE
+}
+
 sub ARG_TRANSLATIONS {
     shift->SUPER::ARG_TRANSLATIONS(),
     n => 'non-interactive',
