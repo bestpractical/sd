99a438df879476209cb732004e8664a3d41f63a1
franck cuny <franck@lumberjaph.net>
2009-09-15 15:26:59 +0200

    integrate comment and update

--- lib/App/SD/Replica/redmine/PushEncoder.pm
+++ lib/App/SD/Replica/redmine/PushEncoder.pm
@@ -70,6 +70,23 @@ sub integrate_ticket_update {
         { isa => 'Prophet::Change' },
         { isa => 'Prophet::ChangeSet' }
     );
+    my $remote_ticket_id
+        = $self->sync_source->remote_id_for_uuid( $change->record_uuid );
+    my $attr = $self->_recode_props_for_integrate($change);
+
+    my $ticket = Net::Redmine::Ticket->load(
+        connection => $self->sync_source->redmine->connection,
+        id         => $remote_ticket_id
+    );
+    for (qw/subject description /) {
+        $ticket->$_( $attr->{$_} ) if $attr->{$_};
+    }
+    if ( $attr->{state} ) {
+        $ticket->status("Open")  if $attr->{state} eq 'open';
+        $ticket->status("Closed") if $attr->{state} eq 'closed';
+    }
+    $ticket->save;
+    return $remote_ticket_id;
 }
 
 sub integrate_ticket_create {
@@ -82,12 +99,27 @@ sub integrate_ticket_create {
     my $attr = $self->_recode_props_for_integrate($change);
     my $ticket = $self->sync_source->redmine->create(ticket => $attr);
     # TODO error
-    return $new->{id};
+    return $ticket->{id};
 }
 
 sub integrate_comment {
     my $self = shift;
-    my ($change) = validate_pos( @_, { isa => 'Prophet::Change' } );
+    my ( $change, $changeset ) = validate_pos(
+        @_,
+        { isa => 'Prophet::Change' },
+        { isa => 'Prophet::ChangeSet' }
+    );
+
+    my %props = map { $_->name => $_->new_value } $change->prop_changes;
+    my $ticket_id
+        = $self->sync_source->remote_id_for_uuid( $props{'ticket'} );
+    my $ticket = Net::Redmine::Ticket->load(
+        connection => $self->sync_source->redmine->connection,
+        id         => $ticket_id
+    );
+    $ticket->description( $props{'content'} );
+    $ticket->save;
+    return $ticket_id;
 }
 
 sub _recode_props_for_integrate {
@@ -114,4 +146,5 @@ sub _recode_props_for_integrate {
 
 __PACKAGE__->meta->make_immutable;
 no Any::Moose;
+
 1;
