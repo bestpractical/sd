47ea794bfa20efa5df16b3a6b367f2e2930ca867
Jesse Vincent <jesse@bestpractical.com>
2009-10-06 17:04:32 -0400

    Update SD to use the new database_bonjour_name API provided by prophet

--- lib/App/SD/Server.pm
+++ lib/App/SD/Server.pm
@@ -1,6 +1,23 @@
 package App::SD::Server;
 use base 'Prophet::Server';
 
+
+
+=head2 database_bonjour_name
+
+Returns the name this database should use to announce itself via bonjour
+
+=cut
+
+sub database_bonjour_name {
+    my $self = shift;
+    my $name = $self->app_handle->setting( label => 'project_name' )->get->[0];
+    my $uuid = $self->handle->db_uuid;
+    return "$name ($uuid)";
+
+}
+
+
 sub css {
     return shift->SUPER::css(@_), "/static/sd/css/main.css";
 }
