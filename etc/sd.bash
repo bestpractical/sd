f6382766737c7d96ec7c6c286a94b9bbe8d90ab0
Kevin Falcone <falcone@bestpractical.com>
2010-01-11 09:22:46 -0500

    The git sd completion wasn't working
    it either stole all of the git completion or it was never noticed

--- etc/sd.bash
+++ etc/sd.bash
@@ -1,13 +1,7 @@
 # add this to your bash_completion.d directory
-
-# handle the multi-word git sd command specially
-function _gitsd_() { 
-    _prophet_ "git sd" 
-}
 function _prophet_()
 {
     COMPREPLY=($($1 _gencomp ${COMP_WORDS[COMP_CWORD]}))
 }
 
 complete -F _prophet_ sd
-complete -F _gitsd_ git sd
